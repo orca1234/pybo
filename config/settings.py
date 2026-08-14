12f0baf18b6e5cd0216f66673920918d415cf952
orca1234 <kimdongju88690009@gmail.com>
2026-08-14 11:43:57 +0900

    secrete_key secure

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ls&1oied3t*np!^y6664m!j2-l%ie=8g$w-bjk_pjryzd#3y6b'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-임시값-로컬용')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
